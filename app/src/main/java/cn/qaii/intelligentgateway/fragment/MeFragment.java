9dac97e5fc84a5eb3dc00c04cbf08ab48542608e
kissks041711 <13964238082@163.com>
2016-03-25 09:51:34 +0800

    "我的"页面更改

--- app/src/main/java/cn/qaii/intelligentgateway/fragment/MeFragment.java
+++ app/src/main/java/cn/qaii/intelligentgateway/fragment/MeFragment.java
@@ -1,25 +1,58 @@
 package cn.qaii.intelligentgateway.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import cn.qaii.intelligentgateway.R;
 import cn.qaii.intelligentgateway.base.BaseFragment;
+import cn.qaii.viewutil_lib.swipe.SwipeRefreshLayout;
+import cn.qaii.viewutil_lib.swipe.SwipeRefreshLayout.OnRefreshListener;
 
 /**
  * cn.qaii.intelligentgateway
  * 功能说明
  * Created by xiuge on 2016/3/16 10:34.
  */
-public class MeFragment extends BaseFragment {
+public class MeFragment extends BaseFragment implements OnRefreshListener,View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_me,null);
+        //initView(view);
+        //initEvent();
+        return view;
+
     }
+    /*private void initEvent(){
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getContext(), SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }*/
+//    private void initView(View view){
+//
+//        setting=(LinearLayout)view.findViewById(R.id.setting);
+//    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onRefresh() {
+
     }
 }
